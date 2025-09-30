25b3213178d2e2b3344c484cc96d062cbea1f50d
Fardzan <144192213+fardzan@users.noreply.github.com>
2025-09-30 12:10:35 +0700

    Add files via upload

---
+++ Pertemuan(2)/TugasPertemuan2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "Nomor 1 - Penjumlahan";
+	cout << "\nMasukkan 2 angka" << endl;
+	cout << "Nlai A = ";
+	cin >> a;
+	cout << "\nNilai B = ";
+	cin >> b;
+	cout << "\nA + B = " << a+b;
+	cout << "\nHasil A + B adalah: " << a+b << endl;
+	
+	cout << "\nNomor 2 - Pengurangan";
+	cout << "\nMasukkan 2 angka" << endl;
+	cout << "Nlai A = ";
+	cin >> a;
+	cout << "\nNilai B = ";
+	cin >> b;
+	cout << "\nA - B = " << a-b;
+	cout << "\nHasil A - B adalah: " << a-b << endl;
+	
+	cout << "\nNomor 3 - Perkalian";
+	cout << "\nMasukkan 2 angka" << endl;
+	cout << "Nlai A = ";
+	cin >> a;
+	cout << "\nNilai B = ";
+	cin >> b;
+	cout << "\nA * B = " << a*b;
+	cout << "\nHasil A * B adalah: " << a*b;
+	
+	return 0;
+}
